427953ac56f565834f297b2b5ab14b7ac4f27175
Jarmo Isotalo <jamo@isotalo.fi>
2018-12-27 00:04:37 +0200

    Simplify and autoformat

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
 const token = process.env.BOT_TOKEN
-const pasteTextFi = 'Valitse Netbeansin ylävalikosta "TMC" -> "Send code to TMC Pastebin" ja valitse avautuvasta ikkunasta "Send". Tämän jälkeen saat linkin koodiisi, jonka voit kopioida ja liittää tänne.'
+const pasteTextFi =
+  'Valitse Netbeansin ylävalikosta "TMC" -> "Send code to TMC Pastebin" ja valitse avautuvasta ikkunasta "Send". Tämän jälkeen saat linkin koodiisi, jonka voit kopioida ja liittää tänne.'
 
 module.exports = async (req, res) => {
-
+  res.setHeader('Content-Type', 'application/json')
   if (req.method === 'POST') {
     let body = ''
     req.on('data', data => {
       body += data
     })
-    req.on('end', async () =>{
+    req.on('end', async () => {
       return await handleMessage(JSON.parse(body), res)
     })
   } else {
-    res.end(`hello`);
+    res.end(`hello`)
     return
   }
 }
 
-async function handleMessage(body, res) {
-  res.setHeader('Content-Type','application/json');
-  console.log('got req:', JSON.stringify(body))
-  msg = {}
-  if (body.message.reply_to_message &&  body.message.reply_to_message.message_id) {
-    msg['reply_to_message_id'] = body.message.reply_to_message.message_id
+async function handleMessage({ message }, res) {
+  console.log('got req:', JSON.stringify(message))
+  const msg = {}
+  if (message.reply_to_message && message.reply_to_message.message_id) {
+    msg['reply_to_message_id'] = message.reply_to_message.message_id
   }
-  if (body.message.text === '/paste') {
+  if (message.text === '/paste') {
     let text = ''
-    if (body.message.reply_to_message && body.message.reply_to_message.from.username) {
-      text = `@${body.message.reply_to_message.from.username} `
+    if (message.reply_to_message && message.reply_to_message.from.username) {
+      text = `@${message.reply_to_message.from.username} `
     }
     text += pasteTextFi
-    msg = Object.assign(msg, {
+    Object.assign(msg, {
       method: 'sendMessage',
-      chat_id: body.message.chat.id,
-      text: text})
-
-    if (body.message.reply_to_message && body.message.reply_to_message.message_id) {
-      msg['reply_to_message_id'] = body.message.reply_to_message.message_id
-    }
-
-    console.log('sending:', JSON.stringify(msg))
-    res.end(JSON.stringify(msg))
-  } else if (body.message.text === '/jamo-test') {
-
-    msg = Object.assign(msg, {method: 'sendMessage',
-      chat_id: body.message.chat.id,
-      text: JSON.stringify(body)
+      chat_id: message.chat.id,
+      text: text,
     })
-    console.log('sending:', JSON.stringify(msg))
-    res.end(JSON.stringify(msg))
-  } else {
-    res.end('')
+  } else if (message.text === '/jamo-test') {
+    Object.assign(msg, {
+      method: 'sendMessage',
+      chat_id: message.chat.id,
+      text: JSON.stringify(message),
+    })
   }
+  console.log('sending:', JSON.stringify(msg))
+  res.end(JSON.stringify(msg))
 }
