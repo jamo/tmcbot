4579bfd22ccf909eb947c3616e4c10be488604bd
ijarmar <43422419+ijarmar@users.noreply.github.com>
2019-12-31 16:53:51 +0200

    Update messages.js
    Co-Authored-By: Jarmo Isotalo <jarmo@isotalo.fi>

--- messages.js
+++ messages.js
@@ -3,7 +3,7 @@ const pasteTextFi =
 const ideLogTextFi =
   'Saat Netbeanssin IDE login näin: 1. Avaa Netbeans. 2. Aiheuta ongelma uudestaan. 3. Valitse Netbeanssin yläpalkista "View" -> "IDE Log". 4. Kopioi avautuvasta paneelista kaikki teksti ja liitä se vastaukseesi.'
 const chmodTextFi =
-  'Suorita päätteessä komento chmod +x /Users/ruusukivi/.config/tmc/apache-maven-3.5.4/bin/mvn Pääte/komentorivi löytyy painamalla command + välilyönti ja kirjoittamalla Pääte (eng. Terminal)'
+  'Suorita päätteessä komento `chmod +x $HOME/.config/tmc/apache-maven-3.5.4/bin/mvn` Pääte/komentorivi löytyy painamalla command + välilyönti ja kirjoittamalla Pääte (eng. Terminal)'
 
 module.exports = {
   pasteTextFi,
