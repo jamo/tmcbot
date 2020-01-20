dd08b8fb290b4a0ce3e2ad3e6c0b4c875330b690
HoolaBoola <46967490+HoolaBoola@users.noreply.github.com>
2020-01-20 22:58:24 +0200

    Add English versions
    Add English versions of the helper texts for those who are Finnish-impaired

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const pasteTextFi =
 
 const ideLogTextFi = 'Saat Netbeanssin IDE login näin: 1. Avaa Netbeans. 2. Aiheuta ongelma uudestaan. 3. Valitse Netbeanssin yläpalkista "View" -> "IDE Log". 4. Kopioi avautuvasta paneelista kaikki teksti ja liitä se vastaukseesi.'
 
+const pasteTextEn = 'In Netbeans, click "TMC" -> "Send code to TMC Pastebin" from the top row. Click "Send" in the opened window. This will give you a link to your code that you can copy and paste here.'
+
+const ideLogTextEn = 'You can retrieve the Netbeans IDE log through these steps: 1. Open Netbeans. 2. Replicate the problem. 3. From the Netbeans top row, choose "Wiev" -> "IDE log". 4. Copy all the text from the opened panel, paste it to a pastebin service (such as pastebin.org), and send the link here.'
+
 module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   if (req.method === 'POST') {
@@ -56,6 +60,28 @@ async function handleMessage(message , res) {
       text = `@${message.reply_to_message.from.username} `
     }
     text += ideLogTextFi
+    Object.assign(msg, {
+      method: 'sendMessage',
+      chat_id: message.chat.id,
+      text: text,
+    })
+  } else if (/!paste-en/.test(message.text)) {
+    let text = ''
+    if (message.reply_to_message && message.reply_to_message.from.username) {
+      text = `@${message.reply_to_message.from.username} `
+    }
+    text += pasteTextEn
+    Object.assign(msg, {
+      method: 'sendMessage',
+      chat_id: message.chat.id,
+      text: text,
+    })
+  } else if (/!idelog-en/.test(message.text)) {
+    let text = ''
+    if (message.reply_to_message && message.reply_to_message.from.username) {
+      text = `@${message.reply_to_message.from.username} `
+    }
+    text += ideLogTextEn
     Object.assign(msg, {
       method: 'sendMessage',
       chat_id: message.chat.id,
